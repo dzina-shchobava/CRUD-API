d43b5a5094d46cccb2fc128a725df7d93e69c774
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-19 11:31:32 +0300

    feat: add postUser function

---
+++ src/controllers/post.ts
@@ -0,0 +1,30 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { createUser } from "../models/usersModel.js";
+import { getPostData } from "../utils/getPostData.js";
+
+export const postUser = async (request: IncomingMessage, response: ServerResponse) => {
+  try {
+    const body = await getPostData(request);
+
+    const { username, age, hobbies } = JSON.parse(body as string);
+
+    const user = {
+      username,
+      age,
+      hobbies: [
+        ...hobbies
+      ]
+    }
+
+    if (username && age ) {
+      const newUsers = await createUser(user);
+      response.writeHead(201,{ 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(newUsers));
+    } else {
+      response.writeHead(400,{ 'Content-Type': 'application/json' });
+      response.end('Request does not contain required fields');
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
